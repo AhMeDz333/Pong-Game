c88b1c176817083c895949f00aa8f4482b1e4a17
ahmadz <ahmed.ashraf33@yahoo.com>
2016-08-14 17:39:09 +0200

    Added a script to instantly deploy apk to my device.

---
+++ test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+adb connect 192.168.1.101:5555
+adb shell am force-stop com.sample.pong
+adb shell am start -a android.intent.action.DELETE -d package:com.sample.pong
+adb shell input tap 580 760
+adb install Export/android/release/bin/bin/Pong-debug.apk
+adb shell am start -n com.sample.pong/com.sample.pong.MainActivity
